85246968a96cddbaff4f043f69f6d3cb18a5b3fa
J <59581793+daechi73@users.noreply.github.com>
2023-04-16 23:19:06 -0400

    .

--- src/index.js
+++ src/index.js
@@ -11,3 +11,21 @@ fetch(
   .then(function (response) {
     img.src = response.data.images.original.url;
   });
+
+document.querySelector("button").addEventListener("click", () => {
+  //console.log("clicking");
+  const img2 = document.createElement("img");
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=en7dMyIj3inRfEXKjj5pj9Hz0CjYPUx1&s=cats",
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img2.src = response.data.images.original.url;
+    });
+  document.body.appendChild(img2);
+});
